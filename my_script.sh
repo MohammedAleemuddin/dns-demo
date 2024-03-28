8c52518df950d41a120f5db2276d33c5a1f5da76
MohammedAleemuddin <163118227+MohammedAleemuddin@users.noreply.github.com>
2024-03-28 17:57:41 +0530

    Update and rename my_script.sh to index.html

--- my_script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo su
-yum update -y
-yum install -y httpd
-
-systemctl start httpd
-systemctl enable httpd
-echo "<h1> Hello world from $(hostname -f)</h1>" > /var/www/html/index.html
