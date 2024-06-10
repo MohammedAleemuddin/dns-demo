1341289067d5fe91df05be413c23021a3de01645
MohammedAleemuddin <163118227+MohammedAleemuddin@users.noreply.github.com>
2024-06-10 18:49:51 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,14 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return 'Hello, Broadlume!'
-    
 
-@app.route('/health')
-def health_check():
-    return jsonify(status='healthy'), 200
-
-# No app.run() here for production
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
